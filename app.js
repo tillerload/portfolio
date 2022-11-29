f9ecdcbdf8261ac880d6d2d251ba58f3ef4007ab
tillerload <tyler.george.work@gmail.com>
2022-11-29 01:41:28 -0330

    added images and started media queries

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-
-const portfolio = {}
-
-
-
+// create namespace and init
+// create a function that uses the doc to select the submit button
+// create an event listener that appends a blank string over my input forms once clicked
 
 
-portfolio.formClear = () => {
 
+
+// This function that resets the field forms is from https://help.formspree.io/hc/en-us/articles/1500009404742-How-to-clear-a-form-after-submission and modified for my site.
+
+
+const portfolio = {};
+
+// WIP DOES NOT WORK HEHE
+portfolio.clearForm = () => {
+  const contactForm = document.querySelector('#contactForm');
+  window.onbeforeunload = () => {
+    contactForm.reset();
+  }
 }
 
-
-
-portfolio.init()
