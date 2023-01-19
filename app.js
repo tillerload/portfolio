cf7829fc1d5b17ca7005d05b14b9273e6036e906
tillerload <tyler.george.work@gmail.com>
2023-01-19 00:35:11 -0330

    Added Carousel its rough but works

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-// create namespace and init
-// create a function that uses the doc to select the submit button
-// create an event listener that appends a blank string over my input forms once clicked
-
-
-
 
 // This function that resets the field forms is from https://help.formspree.io/hc/en-us/articles/1500009404742-How-to-clear-a-form-after-submission and modified for my site.
 
 
 const portfolio = {};
 
-// WIP DOES NOT WORK HEHE
 portfolio.clearForm = () => {
   const contactForm = document.querySelector('#contactForm');
   window.onbeforeunload = () => {
@@ -18,3 +11,21 @@ portfolio.clearForm = () => {
   }
 }
 
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
