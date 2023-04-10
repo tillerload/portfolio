3dc5152d36060fdaf00f7e8d9135cab89ff97cce
tillerload <tyler.george.work@gmail.com>
2023-04-10 21:19:56 -0230

    implemented drop down for nav bar but its kinda wonky

--- app.js
+++ app.js
@@ -16,3 +16,18 @@ buttons.forEach(button => {
     delete activeSlide.dataset.active
   })
 })
+
+// FOR NAV BAR
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navMenu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".navItem").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
